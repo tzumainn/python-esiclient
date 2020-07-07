4f7a630769300392300dce984f8683faa0f02c0d
Naved Ansari <naved001@gmail.com>
2020-07-07 16:02:42 -0400

    This patch enables travis ci and runs some code linting tools
    .

---
+++ ci/pylint_tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd "$(git rev-parse --show-toplevel)"
+pylint \
+  --disable=all \
+  --enable=undefined-variable \
+  --enable=unused-variable \
+  --enable=unused-import \
+  --enable=wildcard-import \
+  --enable=signature-differs \
+  --enable=arguments-differ \
+  --enable=missing-docstring \
+  --enable=logging-not-lazy \
+  --enable=reimported \
+  $(./ci/list_tracked_pyfiles.sh)
